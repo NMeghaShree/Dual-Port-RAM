83772f6ca0edb8fc6beec5e6c466a193af193ee7
N.Megha Shree <98016085+NMeghaShree@users.noreply.github.com>
2025-01-19 20:20:21 +0530

    Update dual_port_ram.v

--- Dual Port RAM Project.srcs/sources_1/new/dual_port_ram.v
+++ Dual Port RAM Project.srcs/sources_1/new/dual_port_ram.v
@@ -20,41 +20,37 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module dual_port_ram(
-    //it has two sets of address and data bus
-    input rst,
-    input [7:0] data_a, data_b, // 8-bit data buses a and b
-    input [5:0] addr_a, addr_b, // 6-bit address buses a and b
-    input we_a, we_b,           // we_a=0 implies read enable and we_a=1 implies write enable, same for we_b
-    input clk,
-    output reg [7:0] q_a, q_b  // output data of two ports a and b
+module dual_port_RAM(
+    // Two sets of address and data buses
+    input rst,                     // Reset signal
+    input [7:0] data_a, data_b,    // 8-bit data buses a and b
+    input [5:0] addr_a, addr_b,    // 6-bit address buses a and b
+    input we_a, we_b,              // Write enable for ports a and b
+    input clk,                     // Clock signal
+    output reg [7:0] q_a, q_b      // Output data for ports a and b
     );
-    
-    reg [7:0] RAM[63:0];   // 64 x 8 bit RAM
-    integer i;
-    always@( posedge clk)
-   
-    begin
-        if(we_a)           // when write is enabled
-        begin
-            RAM[addr_a]<=data_a;    //writing data into memory 
-        end
-        else         // read is enabled
-        begin
-            q_a<=RAM[addr_a];       
-        end
-   end
-    always@( posedge clk)
-        begin 
-    
-   
-        if(we_b)           // when write is enabled
-        begin
-            RAM[addr_b]<=data_b;    //writing data into memory 
-        end
-        else           // read  is enabled
-        begin
-            q_b<=RAM[addr_b];       
+
+    reg [7:0] RAM[63:0];           // 64 x 8-bit RAM
+
+    always @(posedge clk) begin
+        if (rst) begin
+            q_a <= 8'b0;           // Reset output for port A
+        end else if (we_a) begin
+            RAM[addr_a] <= data_a; // Write operation for port A
+        end else begin
+            q_a <= RAM[addr_a];    // Read operation for port A
         end
     end
+
+    always @(posedge clk) begin
+        if (rst) begin
+            q_b <= 8'b0;           // Reset output for port B
+        end else if (we_b) begin
+            RAM[addr_b] <= data_b; // Write operation for port B
+        end else begin
+            q_b <= RAM[addr_b];    // Read operation for port B
+        end
+    end
+
 endmodule
+
